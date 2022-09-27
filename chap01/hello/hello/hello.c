a37d0868ae75899bfa3315f420715f35d532efea
kimjunghyun2 <jhkim5381@gmail.com>
2022-09-27 12:39:08 +0900

    깃헙테스트3

---
+++ chap01/hello/hello/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void) {
+	printf("******************************\n");
+	printf("Let's study C/C++ Progormaing \n");
+	printf("******************************\n");
+	return 0;
+}
---
+++ chap01/test0927/test0927/ch01_39p.c
@@ -0,0 +1,9 @@
+#include <stdio.h> //전처리기 
+int main(void) {
+	printf("***********************************\n");
+	printf("   Let's study C/C++ progormaing   \n");
+	printf("***********************************\n");
+	return 0;// return을 안받으므로 사실 int나 void로 받아도 상관없음 
+} //특수함수 main()
+//함수만들기
+//리턴타입 함수명(매개타입 매개변수 ....){함수 본체}
